d83da0f991b8c546e9e2b1a447734c55b402d061
Alex Welton <alexpwelton@gmail.com>
2013-11-19 06:19:40 -0500

    just pull this

---
+++ src/ParseFASTA.py
@@ -0,0 +1,14 @@
+##
+#
+# FILE: ParseFASTA.py
+#
+# AUTHORS: Alex Welton and Varun Ravishanker
+#
+# DESCRIPTION: Contains functionality for opening and reading
+# a FASTA file, and then parsing the returned data into a useful
+# format.
+#
+# Based on code authored by Sara Thiebaud (code was distributed
+# with class materials for COSC 75 Homework 1
+#
+
