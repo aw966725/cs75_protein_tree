d0ed0639a671366c65b8914d333fb8dc5ff3cb81
Alex Welton <alexpwelton@gmail.com>
2013-11-19 05:17:28 -0500

    updated

---
+++ src/Input.py
@@ -0,0 +1,14 @@
+##
+#
+# FILE: Input.py
+#
+# AUTHORS: Alex Welton and Varun Ravishanker
+#
+# DESCRIPTION: Contains functionality for reading FASTA
+# files and parsing the input data into useful format.
+#
+
+
+def read_FASTA(filename):
+    return
+
