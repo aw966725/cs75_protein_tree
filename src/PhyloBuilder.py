fe313d1f18d48d3dd9729dd918cdeef598aaa4e7
Alex Welton <alexpwelton@gmail.com>
2013-11-19 10:13:06 -0500

    updated more

---
+++ src/PhyloBuilder.py
@@ -0,0 +1,13 @@
+##
+#
+# FILE: PhyloBuilder.py
+#
+# AUTHORS: Alex Welton and Varun Ravishanker
+#
+# DESCRIPTION: The main program - builds a phylogenetic
+# tree from protein gene analysis.
+#
+
+import GUI
+import ParseFASTA
+import Matching
