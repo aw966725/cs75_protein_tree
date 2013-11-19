6795b46e1c0c661a143dd278e00a8ffd7b7777b7
Alex Welton <alexpwelton@gmail.com>
2013-11-19 11:02:37 -0500

    added alignment

---
+++ src/Alignment.py
@@ -0,0 +1,9 @@
+##
+#
+# FILE: Alignment.py
+#
+# AUTHORS: Alex Welton and Varun Ravishanker
+#
+# DESCRIPTION: Contains functionality for implementing
+# subsequence alignment to construct a phylogenetic tree.
+#
