5e078e1a96130cedab7d9575b7516b7c5e49e3ec
Varun Ravishanker <varun.ravishanker@gmail.com>
2013-11-24 01:36:24 -0500

    Updated GUI.py to include sample drawing code

--- src/GUI.py
+++ src/GUI.py
@@ -10,14 +10,32 @@
 
 from tkinter import *
 
+class PhyloTree(Frame):
+    
+    def __init__(self, name):
+        Frame.__init__(self, name)
 
-def gui_init():
-    width, height = 1024, 768
+        self.name = name
 
-    root = tkinter.Tk()
-    window = tkinter.Toplevel()
-    canvas = tkinter.Canvas(window, width=width, height=height)
-    canvas.pack()
+        self.initUI()
 
-def get_file_to_open():
-    return tkinter.filedialog.askopenfilename()
+    def initUI(self):
+        
+        self.name.title("Phylogenetic Tree")
+        self.pack(fill=BOTH, expand=1)
+        
+        canvas = Canvas(self)
+        canvas.create_oval(10, 10, 80, 80, outline="blue", fill = "red", width=2)
+
+        canvas.pack(fill=BOTH, expand=1)
+
+def main():
+    
+    root = Tk()
+    root.geometry("640x480+300+300")
+    tree = PhyloTree(root)
+    root.mainloop()
+
+
+if __name__ == '__main__':
+    main()
