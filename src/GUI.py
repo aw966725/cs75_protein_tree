845d24cd6f1867cc4109efc31bf15012ef00561f
Varun Ravishanker <varun.ravishanker@gmail.com>
2013-11-24 02:28:15 -0500

    added text drawing functionality

--- src/GUI.py
+++ src/GUI.py
@@ -8,34 +8,41 @@
 # for Protein Tree project.
 #
 
+WINDOW_WIDTH = 640
+WINDOW_HEIGHT = 480
+
 from tkinter import *
 
 class PhyloTree(Frame):
     
-    def __init__(self, name):
-        Frame.__init__(self, name)
+    def __init__(self, window, clusters):
+        Frame.__init__(self, window)
 
-        self.name = name
+        self.window = window 
 
-        self.initUI()
+        self.initUI(clusters)
 
-    def initUI(self):
+    def initUI(self, clusters):
         
-        self.name.title("Phylogenetic Tree")
+        self.window.title("Phylogenetic Tree")
         self.pack(fill=BOTH, expand=1)
         
         canvas = Canvas(self)
-        canvas.create_oval(10, 10, 80, 80, outline="blue", fill = "red", width=2)
+        startx = 20
+        starty = WINDOW_HEIGHT / 2
+        
+        #width should be text size divided by depth of clusters 
+
+        canvas.create_text(startx, starty, text=clusters, width=80)
 
         canvas.pack(fill=BOTH, expand=1)
 
 def main():
     
     root = Tk()
-    root.geometry("640x480+300+300")
-    tree = PhyloTree(root)
+    root.geometry(str(WINDOW_WIDTH) + "x" + str(WINDOW_HEIGHT)+ "+450+300")
+    tree = PhyloTree(root, "rig")
     root.mainloop()
-
 
 if __name__ == '__main__':
     main()
