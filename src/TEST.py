c95026167ffc1bc5f109786bf818d53b0a5caade
Alex Welton <alexpwelton@gmail.com>
2013-11-25 04:00:19 -0500

    rig

--- src/TEST.py
+++ src/TEST.py
@@ -26,6 +26,9 @@ from BLAST import *
 TEST_FILE_1 = "../datasets/Saccharomyces_cerevisiae.EF4.73.pep.all.fa"
 TEST_FILE_1_SPECIES = "Saccharomyces Cerevisiae"
 
+TEST_FILE_2 = "../datasets/Ciona_intestinalis.KH.73.pep.all.fa"
+TEST_FILE_2_SPECIES = "Ciona Intestinalis"
+
 LOG_FILE_PREFIX = "TEST_"
 LOG_FILE_SUFFIX = ".txt"
 
@@ -43,14 +46,14 @@ def test_gui(logger):
 
 
 # Test loading and parsing a FASTA file
-def test_load_parse(logger):
+def test_load_parse(logger, filename, species):
 
-    logger.debug("Testing FASTA file LOAD and PARSE functionality...")
+    logger.debug("BEGIN testing FASTA file LOAD and PARSE functionality...")
 
-    logger.debug("Filename: %s", TEST_FILE_1)
-    logger.debug("Species: %s", TEST_FILE_1_SPECIES)
+    logger.debug("Filename: %s", filename)
+    logger.debug("Species: %s", species)
     
-    file_reader = FASTAFile(TEST_FILE_1, TEST_FILE_1_SPECIES)
+    file_reader = FASTAFile(filename, species)
 
     num_genes = file_reader.get_num_unique_genes()
     logger.debug("Number of unique genes in %s: %d", file_reader.species, num_genes)
@@ -71,13 +74,37 @@ def test_load_parse(logger):
     logger.debug("DONE testing FASTA file LOAD and PARSE functionality.")
     logger.debug("")
 
-    return
+    return file_reader
 
 
 # Test matching objects
-def test_matching(logger):
+def test_matching(logger, file_list):
 
-    logger.debug("rig")
+    logger.debug("BEGIN testing matching object creation from FASTA record...")
+
+    tree = PhyloTree(file_list)
+
+    logger.debug("Species in Tree:")
+    for index in range(tree.num_species):
+        logger.debug("Species %d: %s", index, tree.species_names[index])
+
+    logger.debug("Here are the first 5 genes in each species and their variants.")
+    for species_index in range(tree.num_species):
+        logger.debug("Species %d: %s", species_index, tree.species[species_index])
+        for gene_index in range(tree.species[species_index].num_genes):
+            logger.debug("  Gene %d: %s", gene_index, tree.species[species_index].genes[gene_index].gene_ID)
+            for variant_index in range(tree.species[species_index].genes[gene_index].num_variants):
+                logger.debug("    Variant %d: %s", variant_index, tree.species[species_index].genes[gene_index].variant_IDs[variant_index])
+
+    logger.debug("Here is all the information on a sample variant.")
+    first_variant = tree.species[0].genes[0].variants[0]
+    logger.debug("Variant ID: %s", first_variant.variant_ID)
+    logger.debug("Peptide Type: %s", first_variant.peptide_type)
+    logger.debug("Chromosome: %s", first_variant.chromosome)
+    logger.debug("Sequence: %s", first_variant.sequence)
+    logger.debug("Parent Gene ID: %s", first_variant.gene_ID)
+
+    logger.debug("DONE testing matching object creation from FASTA record.")
 
     return
 
@@ -110,9 +137,19 @@ def get_logger():
 # Run all tests
 def test_project():
 
+    # Get the logger object for all testing
     logger = get_logger()
 
-    test_load_parse(logger)
+    # Test file parsing and record creation
+    records_list = []
+
+    file_records_1 = test_load_parse(logger, TEST_FILE_1, TEST_FILE_1_SPECIES)
+    file_records_2 = test_load_parse(logger, TEST_FILE_2, TEST_FILE_2_SPECIES)
+
+    records_list.append(file_records_1)
+    records_list.append(file_records_2)
+
+    test_matching(logger, records_list)
 
     return
 
