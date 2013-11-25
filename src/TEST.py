bb59b397e11130097963ae1957870d689d6af30e
Alex Welton <alexpwelton@gmail.com>
2013-11-25 00:26:48 -0500

    updated test

---
+++ src/TEST.py
@@ -0,0 +1,31 @@
+##
+#
+# FILE: TEST.py
+#
+# AUTHORS: Alex Welton and Varun Ravishanker
+#
+# DESCRIPTION: Contains test code for all components in
+# phylogenetic profiling project.
+#
+
+from imports import *
+
+import ParseFASTA
+import Matching
+import NormalLookup
+import BLAST
+import GUI
+import Profiling
+import Phylobuilder
+
+
+def test_project():
+
+    print("rig")
+
+    return
+
+
+if __name__ == '__main__':
+    ## TODO: USER ARGS STUFF HERE
+    test_project()
